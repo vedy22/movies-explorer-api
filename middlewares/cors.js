355faf04a24a308afab5b09fbbcb3db376467ea3
vedy22 <89541794+vedy22@users.noreply.github.com>
2022-07-22 22:32:28 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'https://tatyana22.ru',
-  'http://tatyana22.ru',
+  'https://diplom.nomoredomains.sbs',
+  'http://diplom.nomoredomains.sbs',
   'localhost:3000',
   'http://localhost:3000',
 ];
